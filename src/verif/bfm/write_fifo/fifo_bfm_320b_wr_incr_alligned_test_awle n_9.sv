1dcbc5e61ced07a376e8fd9399601d962fe77ea5
Shreyas T <126799964+Shreyast27@users.noreply.github.com>
2024-03-06 21:49:54 +0530

    Update fifo_bfm_320b_wr_incr_alligned_test_awle n_9.sv

--- src/verif/bfm/write_fifo/fifo_bfm_320b_wr_incr_alligned_test_awle n_9.sv
+++ src/verif/bfm/write_fifo/fifo_bfm_320b_wr_incr_alligned_test_awle n_9.sv
@@ -1,21 +1,21 @@
 `ifndef WRITE_FIFO_SEQUENCE_INCLUDED_
 `define WRITE_FIFO_SEQUENCE_INCLUDED_
 
-class write_fifo_sequence extends base_sequence;
+class fifo_bfm_320b_wr_incr_alligned_test_awle n_9 extends base_sequence;
 
-  `uvm_object_utils(write_fifo_sequence)
+  `uvm_object_utils(fifo_bfm_320b_wr_incr_alligned_test_awle n_9)
    write_fifo_seq_item req;
    
-  function new(string name = "write_fifo_sequence");
+  function new(string name = "fifo_bfm_320b_wr_incr_alligned_test_awle n_9");
     super.new(name);
   endfunction
 
   virtual task body();
     req = write_fifo_seq_item :: type_id :: create("req");
     `uvm_info(get_type_name(),"Before fifo_bfm_320b_wr_incr_alligned_test_awle n_9",UVM_DEFAULT)
-    repeat(1)begin
+  /  repeat(1)begin
       start_item(req);
-      if(!req.randomize() with {req.type_of_pkt==0;req.awlen==9;req.awsize==WRITE_4_BYTES;req.awburst==WRITE_INCR}); 
+    if(!req.randomize() with {req.type_of_pkt==0; req.awlen==9; req.awsize==WRITE_4_BYTES; req.awburst==WRITE_INCR}); 
        begin
           `uvm_fatal("axi4","Rand failed");
        end 
@@ -24,6 +24,5 @@ class write_fifo_sequence extends base_sequence;
   endtask
 
 endclass
-
 
 `endif
