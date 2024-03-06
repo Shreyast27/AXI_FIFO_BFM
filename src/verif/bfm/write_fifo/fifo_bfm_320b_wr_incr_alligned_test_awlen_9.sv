879aeb6c8be4c6b48b39520b4e6c6d14aeb5956c
Shreyas T <126799964+Shreyast27@users.noreply.github.com>
2024-03-06 21:55:55 +0530

    Update and rename fifo_bfm_320b_wr_incr_alligned_test_awle n_9.sv to fifo_bfm_320b_wr_incr_alligned_test_awlen_9.sv

--- src/verif/bfm/write_fifo/fifo_bfm_320b_wr_incr_alligned_test_awlen_9.sv
+++ src/verif/bfm/write_fifo/fifo_bfm_320b_wr_incr_alligned_test_awlen_9.sv
@@ -1,18 +1,18 @@
-`ifndef WRITE_FIFO_SEQUENCE_INCLUDED_
-`define WRITE_FIFO_SEQUENCE_INCLUDED_
+`ifndef FIFO_BFM_320B_WR_INCR_ALLIGNED_TEST_AWLEN_9_INCLUDED_
+`define FIFO_BFM_320B_WR_INCR_ALLIGNED_TEST_AWLEN_9_INCLUDED_
 
-class fifo_bfm_320b_wr_incr_alligned_test_awle n_9 extends base_sequence;
+class fifo_bfm_320b_wr_incr_alligned_test_awlen_9 extends base_sequence;
 
-  `uvm_object_utils(fifo_bfm_320b_wr_incr_alligned_test_awle n_9)
+  `uvm_object_utils(fifo_bfm_320b_wr_incr_alligned_test_awlen_9)
    write_fifo_seq_item req;
    
-  function new(string name = "fifo_bfm_320b_wr_incr_alligned_test_awle n_9");
+  function new(string name = "fifo_bfm_320b_wr_incr_alligned_test_awlen_9");
     super.new(name);
   endfunction
 
   virtual task body();
     req = write_fifo_seq_item :: type_id :: create("req");
-    `uvm_info(get_type_name(),"Before fifo_bfm_320b_wr_incr_alligned_test_awle n_9",UVM_DEFAULT)
+    `uvm_info(get_type_name(),"Before fifo_bfm_320b_wr_incr_alligned_test_awlen_9",UVM_DEFAULT)
   /  repeat(1)begin
       start_item(req);
     if(!req.randomize() with {req.type_of_pkt==0; req.awlen==9; req.awsize==WRITE_4_BYTES; req.awburst==WRITE_INCR}); 
